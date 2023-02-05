318b85a34f7c3623a5189cd91dd69c6d10f1fe40
kyh <kyhslam@gmail.com>
2023-02-05 09:01:27 +0900

    spring security

---
+++ user-service/src/main/java/com/example/userservice/vo/RequestLogin.java
@@ -0,0 +1,20 @@
+package com.example.userservice.vo;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class RequestLogin {
+
+    @NotNull(message = "Email cannot be null")
+    @Size(min = 2, message = "Email not be less than two characters")
+    @Email
+    private String email;
+
+    @NotNull(message = "password cannot be null")
+    @Size(min = 2, message = "password must not be equals or grater than 8o characters")
+    private String password;
+}
