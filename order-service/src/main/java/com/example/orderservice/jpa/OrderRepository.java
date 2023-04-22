57bd065397596101cff25a5065186994abc1d71f
kyh <kyhslam@gmail.com>
2023-04-23 07:49:40 +0900

    init

--- order-service/src/main/java/com/example/orderservice/jpa/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.orderservice.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<OrderEntity, Long> {
-    OrderEntity findByOrderId(String orderId);
-    Iterable<OrderEntity> findByUserId(String userId);
-
-}
---
+++ second-service/src/main/java/com/kyhslam/secondservice/SecondServiceController.java
@@ -0,0 +1,16 @@
+package com.kyhslam.secondservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service";
+    }
+
+}
