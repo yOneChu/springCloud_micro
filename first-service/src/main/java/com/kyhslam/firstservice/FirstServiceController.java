a492d4505d22bbeb0128d43738df617e417342fa
kyh <kyhslam@gmail.com>
2023-04-23 08:09:26 +0900

    cloud init

--- first-service/src/main/java/com/kyhslam/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/kyhslam/firstservice/FirstServiceController.java
@@ -1,11 +1,14 @@
 package com.kyhslam.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 
     @GetMapping("/welcome")
@@ -13,4 +16,9 @@ public class FirstServiceController {
         return "Welcome to the First service";
     }
 
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service";
+    }
 }
--- second-service/src/main/java/com/kyhslam/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/kyhslam/secondservice/SecondServiceController.java
@@ -1,11 +1,14 @@
 package com.kyhslam.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/second-service")
+@Slf4j
 public class SecondServiceController {
 
     @GetMapping("/welcome")
@@ -13,4 +16,9 @@ public class SecondServiceController {
         return "Welcome to the Second service";
     }
 
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service";
+    }
 }
